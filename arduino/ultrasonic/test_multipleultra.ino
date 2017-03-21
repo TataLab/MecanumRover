356b0c67dcb9e6cb35f54c2afbb6d2aa1bf227ba
Jace Riehl <jaceriehl@Jaces-MacBook-Pro.local>
2017-03-21 11:49:01 -0600

    Added multiple ultrasonic rangefinders code

---
+++ arduino/ultrasonic/test_multipleultra.ino
@@ -0,0 +1,84 @@
+/*
+ HC-SR04 Ping distance sensor:
+ VCC to arduino 5v 
+ GND to arduino GND
+ Echo to Arduino pin 7 
+ Trig to Arduino pin 8
+ 
+ This sketch originates from Virtualmix: http://goo.gl/kJ8Gl
+ Has been modified by Winkle ink here: http://winkleink.blogspot.com.au/2012/05/arduino-hc-sr04-ultrasonic-distance.html
+ And modified further by ScottC here: http://arduinobasics.blogspot.com.au/2012/11/arduinobasics-hc-sr04-ultrasonic-sensor.html
+ on 10 Nov 2012.
+ */
+
+
+#define echoPin1 2 // Echo Pin
+#define trigPin1 3 // Trigger Pin
+
+#define echoPin2 5 // Echo Pin
+#define trigPin2 4 // Trigger Pin
+
+#define echoPin3 8// Echo Pin
+#define trigPin3 7 // Trigger Pin
+
+int maximumRange = 200; // Maximum range needed
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
+
+void setup() {
+ Serial.begin (9600);
+ pinMode(trigPin1, OUTPUT);
+ pinMode(echoPin1, INPUT);
+// pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
+ pinMode(trigPin2, OUTPUT);
+ pinMode(echoPin2, INPUT);
+ pinMode(trigPin3, OUTPUT);
+ pinMode(echoPin3, INPUT);
+}
+
+void pollsensors(int echoPin, int trigPin) {
+/* The following trigPin/echoPin cycle is used to determine the
+ distance of the nearest object by bouncing soundwaves off of it. */ 
+ digitalWrite(trigPin, LOW); 
+ delayMicroseconds(2); 
+
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);
+ 
+ //Calculate the distance (in cm) based on the speed of sound.
+ distance = duration/58.2;
+ 
+ if (distance >= maximumRange || distance <= minimumRange){
+ /* Send a negative number to computer and Turn LED ON 
+ to indicate "out of range" */
+ Serial.println("-1");
+ //digitalWrite(LEDPin, HIGH); 
+ }
+ else {
+ /* Send the distance to the computer using Serial protocol, and
+ turn LED OFF to indicate successful reading. */
+ Serial.println(distance);
+ //digitalWrite(LEDPin, LOW); 
+ }
+ 
+ //Delay 50ms before next reading.
+ //delay(50);
+}
+
+void loop() {
+Serial.println("Sensor 1:");
+pollsensors(echoPin1,trigPin1);
+delay(50);
+
+Serial.println("Sensor 2:");
+pollsensors(echoPin2,trigPin2);
+delay(50);
+
+Serial.println("Sensor3:");
+pollsensors(echoPin3,trigPin3);
+delay(50);
+}
+
