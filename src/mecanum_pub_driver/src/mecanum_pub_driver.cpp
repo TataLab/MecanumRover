b28d0ead9e599162fb6e383e0ec6d78f2dce6bd3
Matt Tata <matt.tata@uleth.ca>
2017-03-23 11:20:03 -0600

    added mecanum_pub_driver

---
+++ src/mecanum_pub_driver/src/mecanum_pub_driver.cpp
@@ -0,0 +1,44 @@
+
+
+//a simple interface for a miniq rover over ROS
+
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+
+//initialize ros and set up a node
+ros::init(argc, argv, "mecanum_commands");  
+ros::NodeHandle nh;
+
+//create a publisher and tell it to publish messages on the miniq/cmd_vel topic with a queue size of 100
+ros::Publisher pub=nh.advertise<geometry_msgs::Twist>("mecanum/cmd_vel",100);
+
+
+//set the loop rate of the publisher to 10hz
+ros::Rate rate(10);
+
+while(ros::ok()){
+	
+	//declare the message
+	geometry_msgs::Twist msg;
+
+	//these are the values to send in the twist message
+	msg.linear.x=0.0;
+	msg.linear.y =10.0;
+	msg.linear.z = 0.0;
+	msg.angular.z=0.0;
+	msg.angular.x=0.0;
+	msg.angular.y=0.0;
+
+	//publish the twist message
+	pub.publish(msg);
+
+	rate.sleep();
+	}
+}
+
+
+
