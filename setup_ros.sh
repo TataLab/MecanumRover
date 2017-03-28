27cd90a179b8454a564b405a64f748c35053a51d
Scott Stone <scott.allan.stone@gmail.com>
2017-03-28 11:59:35 -0600

    piece of shit

---
+++ setup_ros.sh
@@ -0,0 +1,8 @@
+source ./devel/setup.bash
+
+export ROBOT_IP=$1
+export MY_IP=$(hostname -I)
+export ROS_MASTER_URI=http://$ROBOT_IP:11311
+export ROS_IP=$MY_IP
+
+roslaunch robot_2dnav kinect_rviz.launch
