5804c3baa1af351ba1876c854fbfcd5e77684a27
Liam R. Mitchell <liam.mitchell@uleth.ca>
2018-03-28 17:26:20 -0600

    modified things for easier connection wirelessly, launch master start on pi and teleop on wireless control

---
+++ src/teleop_control/scripts/init_0.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sshpass -p 'raspberry' ssh pi@10.88.16.237
--- src/teleop_control/scripts/init_1.sh
+++ src/teleop_control/scripts/init_1.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+export ROS_MASTER_URI=http://localhost:11311
+
 rosrun teleop_twist_keyboard teleop_twist_keyboard.py
