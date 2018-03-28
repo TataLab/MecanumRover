2a9a7ff7e66f27d2293231858d035ea5dad30bd2
Liam R. Mitchell <liam.mitchell@uleth.ca>
2018-03-28 17:41:07 -0600

    removed comment on pixy thing, and also changed teleop control because you have to enter the thing manually

--- src/teleop_control/scripts/init_1.sh
+++ src/teleop_control/scripts/init_1.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sshpass -p 'raspberry' ssh pi@10.88.16.237
+
 export ROS_MASTER_URI=http://localhost:11311
 
-rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+#rosrun teleop_twist_keyboard teleop_twist_keyboard.py
