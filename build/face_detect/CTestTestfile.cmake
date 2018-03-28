0166f5bd7bfd658521050ef312d2fa2b3b5e045b
Liam R. Mitchell <liam.mitchell@uleth.ca>
2018-03-28 17:28:05 -0600

    removed catkin stuff

--- build/face_detect/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/coltan/IntroRobotics/MecanumRover/src/face_detect
-# Build directory: /home/coltan/IntroRobotics/MecanumRover/build/face_detect
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
